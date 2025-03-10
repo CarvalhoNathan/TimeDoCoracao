6d16de9ac3e4388f9b763f682120b6bdb24d160a
CarvalhoNathan <lab53@fatec.com>
2025-03-10 10:16:10 -0300

    Finalizando projeto

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("Santos");
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("Futebol");
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("Clube");
+
+Console.ResetColor();
